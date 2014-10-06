81daf6311112007d917339b0fe4c4d929f192f6c
Miyamoto <user1@Sato-member-debian>
2014-10-06 15:17:58 +0900

    replace

---
+++ run-sim.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+WAFDIR=./
+WAF=${WAFDIR}waf
+
+
+#for  speed in 5 10 20 30 40 50 60 70 80
+for speed in 50
+do
+
+#echo "run $WAF --run "fake -speed=$speed -trace""
+#$WAF --run "icc-scenario -speed=$speed -trace" 
+
+#echo "run $WAF --run "fake -speed=$speed -trace -fake""
+#$WAF --run "icc-scenario -speed=$speed -trace -fake" 
+
+echo "run $WAF --run "fake -speed=$speed -csSize=0 -trace ""
+#$WAF --run "icc-scenario -speed=$speed -csSize=0 -trace " --vis 
+$WAF --run "icc-scenario -speed=$speed -csSize=10000000 -trace " --vis 
+done
+
