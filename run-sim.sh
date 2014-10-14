84b0de46b963447cc53f66df18aea2cf41beb007
Miyamoto <user1@Sato-member-debian>
2014-10-14 10:43:18 +0900

    change waypoint files directories

--- run-sim.sh
+++ run-sim.sh
@@ -5,27 +5,35 @@ WAF=${WAFDIR}waf
 
 mkdir results/
 mkdir results/graphs/
+mkdir results/normal/
+mkdir results/proactive/
 
 for  speed in 0 5 10
 do
 
+mkdir results/normal/speed:$speed/
+mkdir results/proactive/speed:$speed/
+
 for distance in 50 100 200
 do 
 
+mkdir results/normal/speed:$speed/distance:$distance/
+mkdir results/proactive/speed:$speed/distance:$distance/
+
+for mobile in 1 2 5 10
+do
+
+mkdir results/normal/speed:$speed/distance:$distance/MN:$mobile
+mkdir results/proactive/speed:$speed/distance:$distance/MN:$mobile
+
 # Creat Directries for trace files
-mkdir results/normal/
-mkdir results/normal/$speed/
-mkdir results/normal/$speed/$distance/
+echo "run $WAF --run "icc-scenario -speed=$speed -smart -mobile=$mobile -trace""
+$WAF --run "icc-scenario -speed=$speed -distance=$distance -smart -mobile=$mobile -trace" 
 
-echo "run $WAF --run "icc-scenario -speed=$speed -smart -trace""
-$WAF --run "icc-scenario -speed=$speed -distance=$distance -smart -trace" 
+echo "run $WAF --run "icc-scenario -speed=$speed -smart -trace -mobile=$mobile -proactive""
+$WAF --run "icc-scenario -speed=$speed -distance=$distance -smart -proactive -mobile=$mobile -trace" 
 
-mkdir results/proactive/
-mkdir results/proactive/$speed/
-mkdir results/proactive/$speed/$distance/
-
-echo "run $WAF --run "icc-scenario -speed=$speed -smart -trace -proactive""
-$WAF --run "icc-scenario -speed=$speed -distance=$distance -smart -proactive -trace " 
+done
 
 done
 
